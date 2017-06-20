892c5249b6b9c202eab1a68fe0edfdc6ae138a7b
jiehwan.park <jiehwan.park@samsung.com>
2017-06-20 21:15:03 +0900

    update libdockzen

--- src/lib/dockzen.h
+++ src/lib/dockzen.h
@@ -1,60 +1,9 @@
-#ifndef __DOCKZEN_H
-#define __DOCKZEN_H
+#ifndef __DOCKZEN_H__
+#define __DOCKZEN_H__
 
-typedef enum {
-	DOCKZEN_ERROR_NONE = 0, /**< Successful */
-	DOCKZEN_ERROR_INVALID_PARAMETER, /**< Invalid parameter */
-	DOCKZEN_ERROR_OUT_OF_MEMORY, /**< Out of memory */
-	DOCKZEN_ERROR_PERMISSION_DENIED, /**< Permission denied */
-	DOCKZEN_ERROR_NOT_SUPPORTED /**< Not supported  */
-} dockzen_error_e;
+#include "dockzen_types.h"
 
-
-/**
- *  ContainersInfo type definition.
- */
-typedef struct{
-	int count;		/**< the counts of containers info */
-	struct {
-		char * id;			/**< Numeric ID which is managed by swam */
-		char * name;		/**< Container name from service */
-		char * image_name;	/**< Docker image name */
-		char * status;		/**< Container Status :  */
-	}container[10];			/**< Max Count constraint */
-}containers_info_s;
-
-/**
- *  ContainersInfo type definition.
- */
-typedef struct{
-	char * container_name;
-	char * image_name;
-}container_update_s;
-
-typedef struct{
-	char * container_name;
-	char * image_name_prev;
-	char * image_name_new;
-	char *status;
-}container_update_res_s;
-
-typedef struct{
-	char * container_name;
-	char * image_name;
-	char * status;
-}container_update_cb_s;
-
-//typedef void (*container_update_cb) (char* status, void* user_data);
-typedef void (*container_update_cb) (container_update_cb_s * status, void * user_data);
-
-
-/**
- *  capi_GetContainersInfo interface.
- *	return created & serviced containers info which is contolled by dockzen-launcher
- */
 int dockzen_get_containers_info(containers_info_s *c_info);
-
-
-int dockzen_update_container(container_update_s *container_update, container_update_res_s *container_update_return, container_update_cb callback, void * user_data);
+int dockzen_update_container(container_update_s *container_update, container_update_res_s *container_update_res, container_update_cb callback, void* user_data);
 
 #endif
--- src/lib/dockzen_types.h
+++ src/lib/dockzen_types.h
@@ -1,10 +1,68 @@
-#ifndef __DOCKZEN_TYPES_H
-#define __DOCKZEN_TYPES_H
+#ifndef __DOCKZEN_TYPES_H__
+#define __DOCKZEN_TYPES_H__
 
-#ifndef API
-#define API __attribute__ ((visibility("default")))
-#endif
+#define MAX_CONTAINER_NUM	(10)
+
+typedef enum {
+	DOCKZEN_ERROR_NONE = 0,				/**< Successful */
+	DOCKZEN_ERROR_INVALID_PARAMETER,	/**< Invalid parameter */
+	DOCKZEN_ERROR_OUT_OF_MEMORY,		/**< Out of memory */
+	DOCKZEN_ERROR_PERMISSION_DENIED,	/**< Permission denied */
+	DOCKZEN_ERROR_NOT_SUPPORTED,		/**< Not supported  */
+} dockzen_error_e;
+
+typedef enum {
+	DOCKZEN_UPDATE_STATE_STARTED = 0,
+	DOCKZEN_UPDATE_STATE_DOWNLOADING,
+	DOCKZEN_UPDATE_STATE_UPDATING,
+	DOCKZEN_UPDATE_STATE_DONE,
+	DOCKZEN_UPDATE_ERROR_UNKNOWN,
+	DOCKZEN_UPDATE_ERROR_NOTSTART,
+	DOCKZEN_UPDATE_ERROR_DOWNLOAD,
+	DOCKZEN_UPDATE_ERROR_UPDATE
+} dockzen_update_e;
 
 
+/**
+ *  ContainersInfo type definition.
+ */
+typedef struct{
+	int count;		/**< the counts of containers info */
+	struct {
+		char * id;			/**< Numeric ID which is managed by swam */
+		char * name;		/**< Container name from service */
+		char * image_name;	/**< Docker image name */
+		char * status;		/**< Container Status :  */
+	}container[MAX_CONTAINER_NUM];			/**< Max Count constraint */
+}containers_info_s;
+
+/**
+ *  Containers update info type definition.
+ */
+typedef struct{
+	char * container_name;
+	char * image_name;
+}container_update_s;
+
+/**
+ *  Containers update response type definition.
+ */
+typedef struct{
+	char * container_name;
+	char * image_name_prev;
+	char * image_name_new;
+	char * status;
+}container_update_res_s;
+
+/**
+ *  typedef container_update_cb
+ */
+typedef struct{
+	char * container_name;
+	char * image_name;
+	char * status;
+}container_update_cb_s;
+
+typedef void (*container_update_cb) (container_update_cb_s *status, void * user_data);
 
 #endif
