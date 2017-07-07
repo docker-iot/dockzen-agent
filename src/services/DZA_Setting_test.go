a751471955321b6591db252186ce485525bfb5bd
sy1124.kim <sy1124.kim@samsung.com>
2017-07-07 14:03:22 +0900

    Create SetServerURL function. In the case of the file does not exist, called the SetServerURL function.
    Signed-off-by: sy1124.kim <sy1124.kim@samsung.com>

--- src/services/DZA_Setting_test.go
+++ src/services/DZA_Setting_test.go
@@ -5,10 +5,20 @@ import (
   "log"
 )
 
-func TestServerURL(t *testing.T) {
-  log.Printf("[TEST] ========== GetServerURL test code ===========")
-  r := GetServerURL("../../data/server_url.json")
+func TestSetServerURL(t *testing.T){
+  var SERVER_URL = "10.113.62.204:4000"
+  r := SetServerURL(SERVER_URL)
 
+  if r == 0 {
+    log.Printf("[TEST] Set ServerURL OK!!")
+  } else {
+    t.Errorf("[TEST] SetServerURL FAIL!!")
+  }
+}
+
+func TestGetServerURL(t *testing.T) {
+  log.Printf("[TEST] ========== GetServerURL test code ===========")
+  r := GetServerURL()
   if r == "" {
     t.Errorf("[TEST] server URL ret = ", r)
   } else {
