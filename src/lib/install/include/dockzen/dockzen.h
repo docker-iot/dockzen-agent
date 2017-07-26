6556fa6baffc1a9955124867e85c360196373402
namsu lee <nshero.lee@samsung.com>
2017-07-26 19:35:39 +0900

    Update dockzen library and modify .gitignore
    dockzen library from dockzen-launcher is old one.
    Newly build for dockzen-launcher as arm and amd64
    change .gitignore file not to update agent binary
    
    Signed-off-by: namsu lee <nshero.lee@samsung.com>

--- src/lib/install/include/dockzen/dockzen.h
+++ src/lib/install/include/dockzen/dockzen.h
@@ -1,5 +1,21 @@
+/**
+ * @file        dockzen.h
+ * @brief       library for providing dockzen API
+
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ * This software is the confidential and proprietary information
+ * of Samsung Electronics, Inc. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Samsung.
+ */
+
 #ifndef __DOCKZEN_H__
 #define __DOCKZEN_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "dockzen_types.h"
 
@@ -7,7 +23,26 @@
 #define API __attribute__ ((visibility("default")))
 #endif
 
+/**
+ * @fn        int dockzen_get_containers_info(containers_info_s *c_info)
+ * @brief     this function to call 'container info'
+ * @param     *c_info		[out] fill containers infomation
+ * @return   int 			return of function
+ */
 API int dockzen_get_containers_info(containers_info_s *c_info);
+/**
+ * @fn        int dockzen_update_container(container_update_s *container_update, container_update_res_s *container_update_res, container_update_cb callback, void* user_data)
+ * @brief     this function to call 'container info'
+ * @param     *container_update			[in] update parameters
+ * @param     *container_update_res		[out] update response
+ * @param     *callback					[inout] callback function to get update status
+ * @param	  *user_data				[in] user token
+ * @return   int 			return of function
+ */
 API int dockzen_update_container(container_update_s *container_update, container_update_res_s *container_update_res, container_update_cb callback, void* user_data);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* __DOCKZEN_H__ */
